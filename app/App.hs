8b8967df5cf53a9b10089115dfce0979b5728131
DaQuirm <leo.daquirm@gmail.com>
2020-02-17 22:07:38 +0100

    The rest of the 🦉

--- app/App.hs
+++ app/App.hs
@@ -1,19 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module App where
 
+import GHC.Generics (Generic)
 import Control.Lens (makeLenses)
 import Control.Monad.Reader (ReaderT)
 import Control.Concurrent.MVar (MVar)
-
+import Data.Aeson (ToJSON)
 import Servant (Handler, ServerT)
 
-data ServiceState = ServiceState { _requestsTotal :: !Int, _bytesTotal :: !Int }
+data ServiceState = ServiceState
+  { _requestsTotal     :: !Int
+  , _payloadBytesTotal :: !Int
+  , _exceptionsTotal   :: !Int
+  } deriving Generic
 
 makeLenses ''ServiceState
 
+instance ToJSON ServiceState
+
 emptyState :: ServiceState
-emptyState = ServiceState { _requestsTotal = 0, _bytesTotal = 0 }
+emptyState = ServiceState { _requestsTotal = 0, _payloadBytesTotal = 0, _exceptionsTotal = 0 }
 
 type AppT m = ReaderT (MVar ServiceState) m
 
--- app/Main.hs
+++ app/Main.hs
@@ -1,85 +1,122 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Main where
 
 import Data.Text (Text, pack)
 import GHC.Generics (Generic)
+import Control.Lens (Lens')
 import Control.Lens.Operators ((^.), (+~))
-import Control.Monad (forM_)
+import Control.Monad (forever)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Reader (ReaderT, ask)
+import Control.Monad.Reader (MonadReader, ReaderT, ask)
 import Control.Monad.Trans.Reader (runReaderT)
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.MVar (MVar, newMVar, modifyMVar_, readMVar)
-import Network.Wai (Middleware, requestMethod)
-import Network.Wai.Handler.Warp (run)
-import Data.Aeson (ToJSON, FromJSON)
+import Network.Wai (Middleware, requestMethod, requestBodyLength, RequestBodyLength(..))
+import Network.Wai.Handler.Warp (runSettings, defaultSettings, setOnException, setPort)
 import Servant
 import Servant.Server (serve)
 import Network.WebSockets (Connection, withPingThread, sendTextData)
 import Servant.API.WebSocket (WebSocket)
-
--- import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
+import Data.Aeson (ToJSON, encode)
 
 import App
 
 appTToHandler :: MVar ServiceState -> AppT Handler a -> Handler a
 appTToHandler stateVar value = runReaderT value stateVar
 
-data Metric = Metric
-    { metricName :: Text
-    , metricValue :: Int
-    } deriving Generic
+data Metric a = Metric
+  { name :: Text
+  , value :: a
+  } deriving Generic
 
-instance ToJSON Metric
+instance ToJSON a => ToJSON (Metric a)
 
 type MetricAPI
-  = "metrics" :>
-    (    Get '[JSON] Metric
-    )
+    =    "metrics" :> Get '[JSON] ServiceState
+    :<|> "metrics" :> "requestsTotal"     :> Get '[JSON] (Metric Int)
+    :<|> "metrics" :> "exceptionsTotal"   :> Get '[JSON] (Metric Int)
+    :<|> "metrics" :> "payloadBytesTotal" :> Get '[JSON] (Metric Int)
+
+getMetric :: Text -> Lens' ServiceState a -> AppT Handler (Metric a)
+getMetric name lens = do
+  stateVar <- ask
+  state <- liftIO $ readMVar stateVar
+  pure $ Metric name $ state ^. lens
 
 metricAPI :: API MetricAPI
 metricAPI
-  = getMetric
-  where
-    getMetric :: AppT Handler Metric
-    getMetric = do
-      stateVar <- ask
-      state <- liftIO $ readMVar stateVar
-      pure $ Metric "test" $ state ^. requestsTotal
+  =    getAllMetrics
+  :<|> getMetric "requestsTotal"     requestsTotal
+  :<|> getMetric "exceptionsTotal"   exceptionsTotal
+  :<|> getMetric "payloadBytesTotal" payloadBytesTotal
+    where
+      getAllMetrics :: AppT Handler ServiceState
+      getAllMetrics = do
+        stateVar <- ask
+        liftIO $ readMVar stateVar
+
+type CrashAPI = "crash" :> Post '[JSON] NoContent
+
+crashAPI :: API CrashAPI
+crashAPI
+  = crashService
+    where
+      crashService :: AppT Handler NoContent
+      crashService = do
+        error "Crash!"
+        pure NoContent
 
 type WebSocketApi = "stream" :> WebSocket
 
 wsServer :: API WebSocketApi
 wsServer = streamData
  where
-  streamData :: MonadIO m => Connection -> m ()
   streamData connection = do
+    stateVar <- ask
     liftIO $ withPingThread connection 10 (pure ()) (pure ())
-    liftIO . forM_ [1..] $ \i -> do
-       sendTextData connection $ pack $ show i
-       threadDelay 1000000
+    liftIO $ forever $ do
+      state <- readMVar stateVar
+      sendTextData connection $ encode state
+      threadDelay 1000000
 
 type ServiceAPI
   = MetricAPI
   :<|> WebSocketApi
+  :<|> CrashAPI
 
 serviceAPI :: Proxy ServiceAPI
 serviceAPI = Proxy
 
 metricMiddleware :: MVar ServiceState -> Middleware
-metricMiddleware stateVar app = \request responseFunc -> do
-  modifyMVar_ stateVar $ \s -> pure $ requestsTotal +~ 1 $ s
+metricMiddleware stateVar app request responseFunc = do
+  let payloadSize = case requestBodyLength request of
+                      ChunkedBody -> 0
+                      KnownLength l -> fromIntegral l
+
+  modifyMVar_ stateVar $ pure . (requestsTotal +~ 1)
+  modifyMVar_ stateVar $ pure . (payloadBytesTotal +~ payloadSize)
   app request responseFunc
+
+exceptionHandler stateVar _ exception = do
+  print exception
+  modifyMVar_ stateVar $ pure . (exceptionsTotal +~ 1)
 
 main :: IO ()
 main = do
     stateVar <- newMVar emptyState
-    let server = metricAPI :<|> wsServer
+    let server = metricAPI :<|> wsServer :<|> crashAPI
         app = serve
             serviceAPI
             (hoistServer serviceAPI (appTToHandler stateVar) server)
-    run 3000 (metricMiddleware stateVar app)
+    runSettings
+      ( setOnException (exceptionHandler stateVar)
+      $ setPort 3000
+        defaultSettings
+      )
+      (metricMiddleware stateVar app)
